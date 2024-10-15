e861b6644ef174110cbee04efdbb7d3c13da9bd0
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2024-10-15 21:31:45 +0200

    chore: enable live updates in the frontend

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/", api.Handler)
 
-	log.Println("Started server on http://localhost:3000")
+	log.Println("Started server and proxy (frontend with live updates).\n\nBackend: http://localhost:3000\nFrontend: http://localhost:8080")
 	err := http.ListenAndServe(":3000", nil)
 
 	if err != nil {
