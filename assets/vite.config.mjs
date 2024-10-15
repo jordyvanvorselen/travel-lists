30ad588854f3f63f29c04fbd31cc1e8f09c88236
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2024-10-15 20:24:34 +0200

    chore: silence vite warning about htmx eval
    Should be safe:
    
    https://github.com/bigskysoftware/htmx/issues/1944

--- assets/vite.config.mjs
+++ assets/vite.config.mjs
@@ -13,6 +13,11 @@ export default defineConfig({
         manualChunks: undefined,
         entryFileNames: 'index.min.js',
         assetFileNames: 'index.min.[ext]',
+      },
+      onwarn: (entry, next) => {
+        if(entry.loc?.file && /htmx\.min\.js$/.test(entry.loc.file) && /Use of eval in/.test(entry.message))
+          return;
+        return next(entry);
       }
     },
     minify: 'esbuild',
