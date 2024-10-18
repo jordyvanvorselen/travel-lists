f80514287c2beae23c306bbb37fa388f136ea07c
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2024-10-18 21:03:20 +0200

    fix: use relative url so assets work in localhost

--- api/router.go
+++ api/router.go
@@ -14,7 +14,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.Static("/web/assets", "/web/assets")
+	e.Static("/web/assets", "web/assets")
 
 	e.GET("/", handlers.HomeHandler{}.Index)
 	e.GET("/create-list", handlers.ListHandler{}.New)
